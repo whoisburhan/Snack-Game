21439db5aea9dd917043506a17308c5c1fbcaad3
Burhan Uddin <whoisburhan@gmail.com>
2021-07-19 10:16:32 -0400

    Control

---
+++ Assets/PlayerAction.cs
@@ -0,0 +1,187 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerAction.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerAction : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerAction()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerAction"",
+    ""maps"": [
+        {
+            ""name"": ""Snack"",
+            ""id"": ""de06e5cb-41b6-4cd0-87e4-18707f063071"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""e22bf7ac-6287-48c1-b847-71aa10cca776"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""66368337-8c10-48b9-9f6f-4fff1693e4ec"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""cd48dafa-16fb-4d9d-bf6b-966eb883f55f"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""3f6606b1-9aa9-4eea-b807-3372473c9601"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""50470ea0-03ba-47fe-95cc-e524085f3877"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""944d700c-c951-4a3b-adb6-dadb52d32840"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""39530ff9-f28a-457f-a8d9-7d7c76dbddfe"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Snack
+        m_Snack = asset.FindActionMap("Snack", throwIfNotFound: true);
+        m_Snack_Movement = m_Snack.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Snack
+    private readonly InputActionMap m_Snack;
+    private ISnackActions m_SnackActionsCallbackInterface;
+    private readonly InputAction m_Snack_Movement;
+    public struct SnackActions
+    {
+        private @PlayerAction m_Wrapper;
+        public SnackActions(@PlayerAction wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Snack_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_Snack; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(SnackActions set) { return set.Get(); }
+        public void SetCallbacks(ISnackActions instance)
+        {
+            if (m_Wrapper.m_SnackActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_SnackActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_SnackActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_SnackActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_SnackActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public SnackActions @Snack => new SnackActions(this);
+    public interface ISnackActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
