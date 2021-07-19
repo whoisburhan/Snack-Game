874da44d9aa33c571dfcbf6f21b6d2548b00027b
Burhan Uddin <whoisburhan@gmail.com>
2021-07-19 16:01:57 -0400

    snack update

--- Assets/Scripts/SnackController.cs
+++ Assets/Scripts/SnackController.cs
@@ -7,8 +7,13 @@ public class SnackController : MonoBehaviour
     private PlayerAction playerActionControl;
 
     [SerializeField] private Rigidbody2D rb2d;
+    [SerializeField] private Transform snackSegmentPrefab;
+    [SerializeField] private GameObject foodNormal;
+    [SerializeField] private GameObject foodBig;
 
     private Vector2 moveDirection =  Vector2.right;
+    private Vector2 pointTopLeft, pointBottomRight;
+    private List<Transform> snackSegments = new List<Transform>();
 
     private void Awake()
     {
@@ -30,7 +35,12 @@ public class SnackController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointTopLeft = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        pointBottomRight = Camera.main.ScreenToWorldPoint(new Vector3(0f, 0f));
+
+        Debug.Log(pointTopLeft + "|" + pointBottomRight);
+
+        snackSegments.Add(this.transform);
     }
 
     // Update is called once per frame
@@ -46,6 +56,52 @@ public class SnackController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Loop();
+
+        for(int i = snackSegments.Count-1; i > 0; i--)
+        {
+            snackSegments[i].position = snackSegments[i - 1].position;
+        }
+
         transform.position = new Vector3(Mathf.Round(transform.position.x) + moveDirection.x, Mathf.Round(transform.position.y) + moveDirection.y, 0f);
+  
+    }
+
+    private void Loop()
+    {
+        float _x = transform.position.x > pointTopLeft.x ? pointBottomRight.x + 1 : transform.position.x;
+        _x = transform.position.x < pointBottomRight.x ? pointTopLeft.x - 1 : _x;
+
+        float _y = transform.position.y > pointTopLeft.y ? Mathf.Round(pointBottomRight.y) + 1 : transform.position.y;
+        _y = transform.position.y < pointBottomRight.y ? Mathf.Round(pointTopLeft.y) - 1 : _y;
+
+        Debug.Log(_x + "|||" + pointTopLeft.x);
+
+        transform.position = new Vector3(Mathf.Round(_x), Mathf.Round(_y), 0f);
+    }
+
+    private void Grow()
+    {
+        GameObject _go = Instantiate(snackSegmentPrefab.gameObject);
+        _go.transform.position = snackSegments[snackSegments.Count - 1].position;
+        snackSegments.Add(_go.transform);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Food"))
+        {
+            Grow();
+            // Add a new Segment
+            // Change Food Pos
+            // Score += 1
+        }
+
+        else if (col.CompareTag("BigFood"))
+        {
+            // Add a new Segment
+            // Deactivate 
+            // Score += 5
+        }
     }
 }
